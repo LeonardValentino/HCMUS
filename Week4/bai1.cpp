013b820400b8595d7922e11b374d946bafa6198b
Leonard Huỳnh <90182360+LeonardCpp@users.noreply.github.com>
2021-11-01 22:58:31 +0700

    Update bai1.cpp

--- Week4/bai1.cpp
+++ Week4/bai1.cpp
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int kiem_tra_chinh_phuong(int n);
+int main()
+{
+    int n;
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
+    int so_chinh_phuong = kiem_tra_chinh_phuong(n);
+    if(so_chinh_phuong == 1)
+        printf("%d la so chinh phuong.", n);
+    else    
+        printf("%d khong la so chinh phuong.", n);
+    return 0;
+}
 
+int kiem_tra_chinh_phuong(int n)
+{
+    int check = 1;
+    if(sqrt(n) != (int)sqrt(n))
+        check = 0;
+    else check = 1;
+    return check;
+}
