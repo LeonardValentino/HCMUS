5dbe1a7a8d7c17eabba615571ac5ca52ce024d7d
Leonard Huỳnh <90182360+LeonardCpp@users.noreply.github.com>
2021-11-03 13:24:06 +0700

    Create Main1.cpp

---
+++ Tochucchuongtrinh/Task1/Main1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    printf("Nhap so nguyen duong: ");
+    scanf("%d", &n);
+    int kiemtra = kiemtra_chinhphuong(n);
+    if(kiemtra == 1)
+        printf("%d la so chinh phuong.",n);
+    else
+        printf("%d khong la so chinh phuong.",n);
+}
