160f3977015a24c400941fce09b962b9abc548b8
LaFraise <90182360+Lafraise11-Dev@users.noreply.github.com>
2021-11-22 23:06:10 +0700

    Create Bai7.cpp

---
+++ Array1D/Bai7.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <math.h>
+
+void nhapMang(int a[], int &n)
+{
+    do
+    {
+        printf("Nhap vao so phan tu: ");
+        scanf_s("%d", &n);
+        if(n < 1 || n > 25)
+            printf("Mang co toi da 25 pha tu. Xin nhap lai.\n");
+    } while (n < 1 || n > 25);
+
+    for(int i = 0; i < n; i++)
+    {
+        do
+        {
+            printf("Nhap phan tu thu %d: ", i + 1);
+            scanf_s("%d", &a[i]);
+            if(abs(a[i]) > 10000)
+                printf("|a[i]| co gia tri tuyet doi nho hon 10000. Xin nhap lai.\n");
+        } while (abs(a[i]) > 10000);
+        
+    }
+}
+
+void xuatMang(int a[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void xuatPhanTuCoTrongAKhongCoTrongB(int A[], int B[], int n, int m)
+{
+    for(int i = 0; i < n; i++)
+    {
+        int flag = 1;
+        for(int j = 0; j < m; j++)
+        {
+            if(A[i] == B[j])
+            {
+               flag = 0;
+               break;
+            }   
+        }
+        
+        if(flag == 1)
+        {
+            printf("%d ", A[i]);
+        }
+    }
+}
+
+bool kiemTraTrung(int A[], int n, int check)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if(check == A[i])
+            return false;
+    }
+    return true;
+}
+
+void ghepHaiMang(int A[], int B[], int C[], int n, int m, int &k)
+{
+    int dem = 0;
+    for(int i = 0; i < n; i++)
+    {
+        C[dem++] = A[i];
+    }
+
+    for(int j = 0; j < m; j++)
+    {
+        if(kiemTraTrung(A,n,B[j]) == true)
+            C[dem++] = B[j];
+    }
+    k = dem;
+}
+
+int main()
+{
+    int A[25], B[25];
+    int n, m;
+    printf("Nhap mang A: \n");
+    nhapMang(A,n);
+    printf("Nhap mang B: \n");
+    nhapMang(B,m);
+
+    printf("Mang A da nhap: ");
+    xuatMang(A,n);
+    printf("Mang B da nhap: ");
+    xuatMang(B,m);
+    printf("Cac phan tu co trong mang A nhung khong co trong mang B: ");
+    xuatPhanTuCoTrongAKhongCoTrongB(A,B,n,m);
+
+    int C[50];
+    int k;
+    ghepHaiMang(A,B,C,n,m,k);
+    printf("\nMang C gom cac phan tu: ");
+    xuatMang(C,k);
+}
