7a915da51f5956d84c70c7afeab234e6f32edf9d
LaFraise <90182360+Lafraise11-Dev@users.noreply.github.com>
2021-11-28 16:00:11 +0700

    Create Bai1,2.cpp

---
+++ String/Bai1,2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string>
+
+int main()
+{
+    char str[100];
+    printf("Nhap vao mot chuoi: ");
+    fflush(stdin);
+    gets(str);
+    printf("\nChuoi ban vua nhap la: ");
+    puts(str);
+    return 0;
+}
