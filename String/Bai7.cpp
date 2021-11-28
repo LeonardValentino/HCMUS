eae6bf816673c920244d98e7b640f14c0577f27d
LaFraise <90182360+Lafraise11-Dev@users.noreply.github.com>
2021-11-28 16:17:59 +0700

    Create Bai7.cpp

---
+++ String/Bai7.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string>
+#include <string.h>
+
+void xoaKyTu(char str[], int index)
+{
+    for(int i = index;i  < strlen(str); i++)
+    {
+        str[i] = str[i + 1];
+    }
+}
+
+void xoaKhoangTrang(char str[])
+{
+    for(int i = 0; i < strlen(str); i++)
+    {
+        if(str[i] == ' ')
+            xoaKyTu(str,i);
+    }
+}
+
+void xuatChuoi(char str[])
+{
+    for(int i = 0; i < strlen(str); i++)
+    {
+        printf("%c", str[i]);
+    }
+}
+
+int main()
+{
+    char str[100];
+    printf("Nhap vao mot chuoi: ");
+    fflush(stdin);
+    gets(str);
+    printf("\nChuoi ban vua nhap la: ");
+    puts(str);
+    
+    xoaKhoangTrang(str);
+    printf("Chuoi sau khi xoa cac ki tu khoang trang: ");
+    xuatChuoi(str);
+    return 0;
+}
