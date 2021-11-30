3f17402f08248b00ac3edce1043fae33f01b39ea
LaFraise <90182360+Lafraise11-Dev@users.noreply.github.com>
2021-11-30 20:48:27 +0700

    Create temp.cpp
    Sửa lại không cần else, 1 lỗi

---
+++ String/temp.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+void xoaKiTu(char str[], int index)
+{
+    for(int i = index; i < strlen(str); i++)
+    {
+        str[i] = str[i + 1];
+    }
+}
+
+void xoaKyTuDacBiet(char str[])
+{
+    for(int i = 0; i < strlen(str); i++)
+    {
+        if((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+        {
+            printf("Ki tu %d hop le.",i);
+        }
+        else
+        {
+            char answer;
+            printf("\nPhat hien ki tu dac biet o vi tri %d, neu ban muon xoa hay nhan 'y': ",i);
+            fflush(stdin);
+            scanf("%c", &answer);
+            if(answer == 'y')
+            {
+                xoaKiTu(str,i);
+                i--;
+            }
+            else
+            {
+                i++;
+            }
+            printf("Chuoi sau khi ban nhap lua chon: ");
+            puts(str);
+        }
+    }
+    printf("Chuoi cuoi cung la: ");
+    puts(str);
+}
+
+int main()
+{
+    char str[100];
+    gets(str);
+    xoaKyTuDacBiet(str);
+    return 0;
+}
