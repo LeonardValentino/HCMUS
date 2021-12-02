891d7cd67ddd49412cd6a7f4de52ac072d45a5d6
LaFraise <90182360+Lafraise11-Dev@users.noreply.github.com>
2021-12-02 21:28:51 +0700

    Create Debai.cpp

---
+++ Array2D/Debai.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("----------------------- Cac chuc nang cua chuong trinh ---------------------");
+    printf("\n| 1. Tinh tong cac phan tu duong trong ma tran.                            |");
+    printf("\n| 2. Dem so luong so nguyen to trong ma tran.                              |");
+    printf("\n| 3. Tim so lon nhat tren bien ma tran.                                    |");
+    printf("\n| 4. Tim so duong nho nhat trong ma tran.                                  |");
+    printf("\n| 5. Liet ke cac dong co chua gia tri am trong ma tran.                    |");
+    printf("\n| 6. Liet ke cac dong chua toan so chan trong ma tran.                     |");
+    printf("\n| 7. Dem so luong gia tri yen ngua tren ma tran.                           |");
+    printf("\n| 8. Dem so luong gia tri hoang hau tran ma tran.                          |");
+    printf("\n| 9. Tong cac phan tu cuc tri tren ma tran.                                |");
+    printf("\n| 10.Tim chu so xuat hien nhieu nhat trong ma tran.                        |");
+    printf("\n| 11.Sap xep cac gia tri bien cua ma tran tang dan theo chieu kim dong ho. |");
+    printf("\n|__________________________________________________________________________|");
+}
