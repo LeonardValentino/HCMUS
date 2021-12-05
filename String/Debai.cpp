717b09924ae64c76a576f48d07c9a593080ce3a2
LaFraise <90182360+Lafraise11-Dev@users.noreply.github.com>
2021-12-05 09:49:21 +0700

    Create Debai.cpp

---
+++ String/Debai.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" ____________________________________________________________________________________");
+    printf("\n|----------------------  Cac chuc nang cua chuong trinh -----------------------------|");
+    printf("\n| 1. Xuat chuoi vua nhap.                                                            |");
+    printf("\n| 2. Xuat chuoi dao.                                                                 |");
+    printf("\n| 3. Xuat moi tu cua chuoi tren mot dong.                                            |");
+    printf("\n| 4. Viet hoa ki tu dau cua moi tu trong chuoi.                                      |");
+    printf("\n| 5. Nhap vao chuoi con, in ra danh sach cac vi tri xuat hien chuoi con trong chuoi. |");
+    printf("\n| 6. Loai bo tat ca khoang trong thua trong chuoi.                                   |");
+    printf("\n| 7. Nhap vao vi tri cua tu, hay cho biet tu nay gom bao nhieu ki tu.                |");
+    printf("\n| 8. Tim danh sach vi tri cac ki tu khong phai ki so hoac ki tu.                     |");
+    printf("\n| 9. Loai bo nhung ki tu khong phai ki so hoac ki tu trong chuoi.                    |");
+    printf("\n|    Truoc khi xoa hoi nguoi dung xem co muon xoa khong.                             |");
+    printf("\n|____________________________________________________________________________________|");
+}   
