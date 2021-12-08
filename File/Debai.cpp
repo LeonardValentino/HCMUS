4978b63ce508306eca2ff257cfbe6235ab1dc509
LaFraise <90182360+LeonardHuynh@users.noreply.github.com>
2021-12-09 00:34:51 +0700

    Create Debai.cpp

---
+++ File/Debai.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n|=================================== Chuc nang cua chuong trinh ===========================================|");
+    printf("\n| 1. Doc file chua 2 so nguyen duong M N va xuat cac so nguyen to trong khoang M N ra file output.txt      |");
+    printf("\n| 2. Doc ma tran tu file va xuat noi dung ghi duoc vao mot file khac                                       |");
+    printf("\n| 3. Doc mot cau truc phan so tu file va xuat phan so ra mot file khac                                     |");
+    printf("\n| 4. Doc mot day phan so va xuat day phan so do ra mot file khac                                           |");
+    printf("\n| 5. Doc mot cau truc diem 2D va xuat diem do ra mot file khac                                             |");
+    printf("\n| 6. Doc mot day diem 2D va xuat day diem 2D do ra mot file khac                                           |");
+    printf("\n| 7. Doc mot cau truc duong tron 2D va xuat duong tron do ra mot file khac                                 |");
+    printf("\n| 8. Doc mot day duong tron 2D va xuat day duong tron 2D do ra mot file khac                               |");
+    printf("\n| 9. Doc mot ma tran phan so va xuat ma tran do ra mot file khac                                           |");
+    printf("\n| 10.Doc mot ma tran diem 2D va xuat ma tran do ra mot file khac                                           |");
+    printf("\n| 11.Doc mot ma tran duong tron 2D va xuat ma tran do ra mot file khac                                     |");
+    printf("\n|__________________________________________________________________________________________________________|");
+}
